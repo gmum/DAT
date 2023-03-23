b3799ba55ddb050ee53933dbe44356ad5503cc45
Mikołaj Sacha <mikolajsacha@gmail.com>
2023-03-23 23:13:08 +0100

    Add 'class_proj'

--- models/dat.py
+++ models/dat.py
@@ -17,11 +17,12 @@ from timm.models.layers import DropPath, to_2tuple
 
 from .dat_blocks import *
 
+
 class TransformerStage(nn.Module):
 
     def __init__(self, fmap_size, window_size, ns_per_pt,
-                 dim_in, dim_embed, depths, stage_spec, n_groups, 
-                 use_pe, sr_ratio, 
+                 dim_in, dim_embed, depths, stage_spec, n_groups,
+                 use_pe, sr_ratio,
                  heads, stride, offset_range_factor, stage_idx,
                  dwc_pe, no_off, fixed_pe,
                  attn_drop, proj_drop, expansion, drop, drop_path_rate, use_dwc_mlp):
@@ -38,7 +39,7 @@ class TransformerStage(nn.Module):
         )
         self.mlps = nn.ModuleList(
             [
-                TransformerMLPWithConv(dim_embed, expansion, drop) 
+                TransformerMLPWithConv(dim_embed, expansion, drop)
                 if use_dwc_mlp else TransformerMLP(dim_embed, expansion, drop)
                 for _ in range(depths)
             ]
@@ -52,10 +53,10 @@ class TransformerStage(nn.Module):
                 )
             elif stage_spec[i] == 'D':
                 self.attns.append(
-                    DAttentionBaseline(fmap_size, fmap_size, heads, 
-                    hc, n_groups, attn_drop, proj_drop, 
-                    stride, offset_range_factor, use_pe, dwc_pe, 
-                    no_off, fixed_pe, stage_idx)
+                    DAttentionBaseline(fmap_size, fmap_size, heads,
+                                       hc, n_groups, attn_drop, proj_drop,
+                                       stride, offset_range_factor, use_pe, dwc_pe,
+                                       no_off, fixed_pe, stage_idx)
                 )
             elif stage_spec[i] == 'S':
                 shift_size = math.ceil(window_size / 2)
@@ -64,17 +65,16 @@ class TransformerStage(nn.Module):
                 )
             else:
                 raise NotImplementedError(f'Spec: {stage_spec[i]} is not supported.')
-            
+
             self.drop_path.append(DropPath(drop_path_rate[i]) if drop_path_rate[i] > 0.0 else nn.Identity())
-        
+
     def forward(self, x):
-        
+
         x = self.proj(x)
-        
+
         positions = []
         references = []
         for d in range(self.depths):
-
             x0 = x
             x, pos, ref = self.attns[d](self.layer_norms[2 * d](x))
             x = self.drop_path[d](x) + x0
@@ -86,19 +86,20 @@ class TransformerStage(nn.Module):
 
         return x, positions, references
 
+
 class DAT(nn.Module):
 
     def __init__(self, img_size=224, patch_size=4, num_classes=1000, expansion=4,
-                 dim_stem=96, dims=[96, 192, 384, 768], depths=[2, 2, 6, 2], 
-                 heads=[3, 6, 12, 24], 
+                 dim_stem=96, dims=[96, 192, 384, 768], depths=[2, 2, 6, 2],
+                 heads=[3, 6, 12, 24],
                  window_sizes=[7, 7, 7, 7],
-                 drop_rate=0.0, attn_drop_rate=0.0, drop_path_rate=0.0, 
-                 strides=[-1,-1,-1,-1], offset_range_factor=[1, 2, 3, 4], 
-                 stage_spec=[['L', 'D'], ['L', 'D'], ['L', 'D', 'L', 'D', 'L', 'D'], ['L', 'D']], 
+                 drop_rate=0.0, attn_drop_rate=0.0, drop_path_rate=0.0,
+                 strides=[-1, -1, -1, -1], offset_range_factor=[1, 2, 3, 4],
+                 stage_spec=[['L', 'D'], ['L', 'D'], ['L', 'D', 'L', 'D', 'L', 'D'], ['L', 'D']],
                  groups=[-1, -1, 3, 6],
-                 use_pes=[False, False, False, False], 
+                 use_pes=[False, False, False, False],
                  dwc_pes=[False, False, False, False],
-                 sr_ratios=[8, 4, 2, 1], 
+                 sr_ratios=[8, 4, 2, 1],
                  fixed_pes=[False, False, False, False],
                  no_offs=[False, False, False, False],
                  ns_per_pts=[4, 4, 4, 4],
@@ -115,24 +116,28 @@ class DAT(nn.Module):
         ) if use_conv_patches else nn.Sequential(
             nn.Conv2d(3, dim_stem, patch_size, patch_size, 0),
             LayerNormProxy(dim_stem)
-        ) 
+        )
+
+        if self.conditional:
+            # TODO parametrize 1000
+            self.class_proj = nn.Linear(dim_stem + 1000, dim_stem)
 
         img_size = img_size // patch_size
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
-        
+
         self.stages = nn.ModuleList()
         for i in range(4):
             dim1 = dim_stem if i == 0 else dims[i - 1] * 2
             dim2 = dims[i]
             self.stages.append(
                 TransformerStage(img_size, window_sizes[i], ns_per_pts[i],
-                dim1, dim2, depths[i], stage_spec[i], groups[i], use_pes[i], 
-                sr_ratios[i], heads[i], strides[i], 
-                offset_range_factor[i], i,
-                dwc_pes[i], no_offs[i], fixed_pes[i],
-                attn_drop_rate, drop_rate, expansion, drop_rate, 
-                dpr[sum(depths[:i]):sum(depths[:i + 1])],
-                use_dwc_mlps[i])
+                                 dim1, dim2, depths[i], stage_spec[i], groups[i], use_pes[i],
+                                 sr_ratios[i], heads[i], strides[i],
+                                 offset_range_factor[i], i,
+                                 dwc_pes[i], no_offs[i], fixed_pes[i],
+                                 attn_drop_rate, drop_rate, expansion, drop_rate,
+                                 dpr[sum(depths[:i]):sum(depths[:i + 1])],
+                                 use_dwc_mlps[i])
             )
             img_size = img_size // 2
 
@@ -147,22 +152,22 @@ class DAT(nn.Module):
                     LayerNormProxy(dims[i + 1])
                 )
             )
-           
-        self.cls_norm = LayerNormProxy(dims[-1]) 
+
+        self.cls_norm = LayerNormProxy(dims[-1])
         self.cls_head = nn.Linear(dims[-1], num_classes)
-        
+
         self.reset_parameters()
-    
+
     def reset_parameters(self):
 
         for m in self.parameters():
             if isinstance(m, (nn.Linear, nn.Conv2d)):
                 nn.init.kaiming_normal_(m.weight)
                 nn.init.zeros_(m.bias)
-                
+
     @torch.no_grad()
     def load_pretrained(self, state_dict):
-        
+
         new_state_dict = {}
         for state_key, state_value in state_dict.items():
             keys = state_key.split('.')
@@ -197,9 +202,9 @@ class DAT(nn.Module):
                     pre_interp = state_value.unsqueeze(0)
                     post_interp = F.interpolate(pre_interp, (H, W), mode='bicubic')
                     new_state_dict[state_key] = post_interp.squeeze(0)
-        
+
         self.load_state_dict(new_state_dict, strict=False)
-    
+
     @torch.jit.ignore
     def no_weight_decay(self):
         return {'absolute_pos_embed'}
@@ -207,12 +212,26 @@ class DAT(nn.Module):
     @torch.jit.ignore
     def no_weight_decay_keywords(self):
         return {'relative_position_bias_table', 'rpe_table'}
-    
+
     def forward(self, x):
         if isinstance(x, list):
             x, query_vector = x
-        
+        else:
+            query_vector = None
+
+        # torch.Size([64, 3, 224, 224]) x
+        # torch.Size([64, 1000]) query
+        # torch.Size([64, 96, 56, 56]) proj
+
         x = self.patch_proj(x)
+
+        if query_vector is not None:
+            x = x.transpose(0, 2, 3, 1)  # 64, 56, 56, 96
+            query_vector = query_vector.unsqueeze(1).unsqeeze(2)  # 64, 1, 1, 1000
+            x = torch.cat((x, query_vector), dim=-1)  # 64, 56, 56, 1096
+            x = self.class_proj(x)  # 64, 56, 56, 96
+            x = x.transpose(0, 3, 1, 2)  # 64, 96, 56, 56
+
         positions = []
         references = []
         for i in range(4):
@@ -225,5 +244,5 @@ class DAT(nn.Module):
         x = F.adaptive_avg_pool2d(x, 1)
         x = torch.flatten(x, 1)
         x = self.cls_head(x)
-        
+
         return x, positions, references
